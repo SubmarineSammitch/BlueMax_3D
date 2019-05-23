738c29d6d97f4ff3a1ee74d796ee142332c94915
Chris <gto389ho@gmail.com>
2019-05-23 00:33:08 -0700

    Adding Player to Map
    Adding player to the map and setting starting position

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject playerStart = new Vector3()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
